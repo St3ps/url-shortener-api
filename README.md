92177aa979e007a0987b29447d26e368bd5fc1f9
Joel <jl.fmst@gmail.com>
2016-10-11 13:30:05 +0000

    finished implementing all proposed functionalities, pushing project to github

---
+++ README.md
@@ -0,0 +1,5 @@
+Hey there, welcome to my Url Shortener Microservice API! 
+
+This was done in context of FCC's Node.js API challenges and a big credit for the code
+should go for Michael, you can find his tutorial here:
+http://lefkowitz.me/thoughts/2016/05/05/men-stack-building-a-url-shortener-with-mongodb-express-and-node-js/
---
+++ routes/index.js
@@ -0,0 +1,91 @@
+var mongo = require('mongodb').MongoClient;
+var config = require('../config.js');
+var shortid = require('shortid');
+var validUrl = require('valid-url');
+
+var mLab = 'mongodb://' + config.db.host + '/' + config.db.name;
+
+exports.urlcatch = function(req, res, next){
+    
+    mongo.connect(mLab, function (err, db) {
+    if (err) {
+      console.log("Unable to connect to server", err);
+    } else {
+      console.log("Connected to server")
+        var collection = db.collection('links');
+        var params = req.params.url;
+        
+        var newLink = function (db, callback) {
+          //Check if it already exists
+          collection.findOne({ "url": params }, { url: 1, short: 1, _id: 0 }, function(err, doc) {
+            if(doc) {
+              console.log("URL already exists in db");
+              res.json({original_url: doc.url, short_url: doc.short_url})
+              } else {
+                  if (validUrl.isUri(params)) {
+                    var shortCode = shortid.generate();
+                    var newUrl = { url: params, short: shortCode };
+                    collection.insert([newUrl]);
+                    res.json({ original_url: params, short_url: "localhost:8080/" + shortCode})
+                  } else {
+                    res.json({ error: "Wrong url format!"});
+                  }
+
+              }
+            }  
+
+          );
+      };
+         
+        newLink(db, function () {
+          console.log('closing db');
+          db.close();
+        });
+    }
+  });
+}
+
+exports.shortr = function (req, res) {
+  
+  mongo.connect(mLab, function(err,db) {
+    if (err) {
+      console.log("Unable to connect to server", err);
+    } else {
+      console.log("Connected to server");
+      
+      var collection = db.collection('links');
+      var params = req.params.short; 
+      console.log(params);
+      var findLink = function(db, callback) {
+        collection.findOne(
+          
+          { "short":params }, {url : 1, _id: 0}, function(err, doc) {
+            if (doc != null) {
+              res.redirect(doc.url);
+            } else {
+              res.json({ error: "No corresponding shortlink found in the database."});
+            }
+          }
+          
+        );
+      };
+      
+      findLink(db, function() {
+        db.close();
+      });
+      
+      
+    }
+    
+    
+    
+  });
+  
+  
+}
+
+
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
